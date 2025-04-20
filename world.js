673bbe0e0894edd122fc1df188a419031d27203e
playvictor15 <127317443+playvictor15@users.noreply.github.com>
2025-04-20 10:54:26 -0300

    Update world.js

--- world.js
+++ world.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.152.2';
 import { criarFoquinho } from './foquinhos.js';
 
 let mixer;
